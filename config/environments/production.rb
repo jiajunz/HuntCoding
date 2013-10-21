4bf240cf978f75f47127986a3f556462ef46fbf7
jiajunz <jiajunzhao14@gmail.com>
2013-10-21 00:07:01 -0700

    precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,9 @@ Jjywebsite::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  # 
+  # compile images
+  # config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
